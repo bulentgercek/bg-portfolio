bf43858db72e1a056aa334c6f0fb9eeb390580a9
Bulent Gercek <bulentgercek@gmail.com>
2023-02-19 23:45:51 +0300

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ server.use(express.json());
 
 server.get("/", (req, res) => {
   res.json({
-    message: "Welcome to the BG Portfolio Server",
+    message: "Welcome to the BG Portfolio Server | Test",
   });
 });
 
