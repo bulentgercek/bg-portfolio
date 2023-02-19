92776da15a2e22bc4458391272f5318f42e23b2f
Bulent Gercek <bulentgercek@gmail.com>
2023-02-20 00:35:56 +0300

    Bash command for automating update process... maybe...

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Change directory to the local repository folder
+cd /path/to/repository
+
+# Update the local repository with changes from the remote repository
+git pull
+
+# Build the server
+cd server
+npm run build
+cd ..
+
+# Restart the server using pm2
+pm2 restart index
