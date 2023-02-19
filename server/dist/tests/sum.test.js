d044eee5f1b35911a04773ec90bb03b66282c98a
Bulent Gercek <bulentgercek@gmail.com>
2023-02-19 23:58:33 +0300

    Some fixes

--- server/dist/tests/sum.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sum_1 = require("./sum");
-test("add 5 + 15 to equal 20", () => {
-    expect((0, sum_1.sum)(5, 15)).toBe(20);
-});
