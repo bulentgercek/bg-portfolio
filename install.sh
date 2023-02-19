9e0d4d5c45c3f3d0e91628b4f7478ce3d307274e
Bulent Gercek <bulentgercek@gmail.com>
2023-02-20 01:15:04 +0300

    Wow:)

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Build Server and Client
+npm run install
+
+echo "Installation is complete. Please run bash update.sh to build and update the project."
--- update.sh
+++ update.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Change directory to the local repository folder
-cd /home/bulentgercek/bg-portfolio
-
 # Update the local repository with changes from the remote repository
 git pull
 
-# Build the server
-cd server
+# Build Server and Client
 npm run build
-cd ..
 
-# Build the client
-cd client
-npm run build
-cd ..
-
-# Restart the server using pm2
-pm2 restart index
+# Check if the PM2 process is running
+if pm2 show index > /dev/null; then
+  # If the process is already running, restart it
+  pm2 restart index
+else
+  # If the process is not running, start it
+  pm2 start server/dist/index.js
+  pm2 save
+fi
