6ec137e60176130467b86ea8d67c26a4060f7e52
Bulent Gercek <bulentgercek@gmail.com>
2023-04-09 14:17:19 +0300

    Axios baseURL->.env (VITE)

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,18 +2,13 @@
  * Axios DAL Functions
  */
 import axios from "axios";
-import { Asset, Category } from "./interfaces";
+import { Category } from "./interfaces";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export namespace Api {
-  // Get Assets
-  export async function getAssets() {
-    const res = await axiosInstance.get<Asset[]>("/assets");
-    return res.data;
-  }
   // Get Categories
   export async function getCategories() {
     const res = await axiosInstance.get<Category[]>("/categories");
