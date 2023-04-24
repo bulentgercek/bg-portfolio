1ee106ad4adfa6c89808a7717d30d6f341938368
Bulent Gercek <bulentgercek@gmail.com>
2023-04-24 14:19:33 +0300

    LandingBanner progress

--- client/src/components/Content/LandingBanner.tsx
+++ client/src/components/Content/LandingBanner.tsx
@@ -14,12 +14,15 @@ const LandingBanner: React.FC = () => {
   const { contentSizeData } = useContext(AppContext);
   return (
     <div id="landing_banner_container" className="flex-none flex-grow-0 self-stretch rounded-2xl border bg-indigo-50">
-      <div id="landing_banner" className="flex flex-row items-center gap-5 border p-10 pl-7">
-        <div>
-          Hello! My name is Bulent Gercek. You can find my past and current notable works on this personal website. I
-          wish to meet you one day. Happy surfing 🤗
-        </div>
-        <div className="font-bold">{Math.round(contentSizeData?.width ?? 0)}</div>
+      <div
+        id="landing_banner"
+        className="flex items-center gap-5 border border-red-500 p-10 pl-7 text-xl text-indigo-900"
+      >
+        <p>
+          <span className="font-bold">Hello! My name is Bulent Gercek.</span> You can find my past and current notable
+          works on this personal website. I wish to meet you one day. Happy surfing 🤗
+          <span className="text-gray-400">{Math.round(contentSizeData?.width ?? 0)}</span>
+        </p>
       </div>
       <div id="landing_banner_back" className=""></div>
     </div>
