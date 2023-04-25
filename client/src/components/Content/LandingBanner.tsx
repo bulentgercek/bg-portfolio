5c30661daf8db98f64b0eae62c64099a1fc40b58
Bulent Gercek <bulentgercek@gmail.com>
2023-04-25 16:58:46 +0300

    landingBanner progress

--- client/src/components/Content/LandingBanner.tsx
+++ client/src/components/Content/LandingBanner.tsx
@@ -1,28 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+
+import bg_photo from "../../assets/bg_photo.png";
+import icon_linkedin from "../../assets/icon_linkedin.svg";
+import icon_github from "../../assets/icon_github.svg";
+import icon_twitter from "../../assets/icon_twitter.svg";
+import { getCustomCSSVariables } from "../../../utils";
 import AppContext from "../../AppContext";
 
 /**
- * Content Area Size Notes
- * 1. min-width: 1200
- * 2. min-width: 1015
- * 3. min-width: 895
- * 4. min-width: 560
- * 5. < 560
+ * Landing Banner Component
  */
-
 const LandingBanner: React.FC = () => {
   const { contentSizeData } = useContext(AppContext);
+  const [isSmall, setIsSmall] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.clear();
+    const contentCSSVariables = getCustomCSSVariables("--content");
+    console.log(contentCSSVariables);
+
+    setIsSmall(contentSizeData ? contentSizeData.width < contentCSSVariables["--content-sm"] : false);
+  }, [contentSizeData]);
+
   return (
     <div id="landing_banner_container" className="flex-none flex-grow-0 self-stretch rounded-2xl border bg-indigo-50">
       <div
         id="landing_banner"
-        className="flex items-center gap-5 border border-red-500 p-10 pl-7 text-xl text-indigo-900"
+        className={`flex ${
+          isSmall ? `flex-col items-start` : `flex-row items-center`
+        }  gap-5 p-10 pl-7 text-xl text-indigo-900`}
       >
-        <p>
-          <span className="font-bold">Hello! My name is Bulent Gercek.</span> You can find my past and current notable
-          works on this personal website. I wish to meet you one day. Happy surfing 🤗
-          <span className="text-gray-400">{Math.round(contentSizeData?.width ?? 0)}</span>
-        </p>
+        <div id="text_n_buttons" className={`flex flex-col gap-5  ${isSmall ? `order-2` : `order-1`}`}>
+          <div id="text">
+            <p>
+              <span className="font-bold">Hello! My name is Bulent Gercek.</span> You can find my past and current
+              notable works on this personal website. I wish to meet you one day. Happy surfing 🤗
+              <span className="text-gray-400">{Math.round(contentSizeData?.width ?? 0)}</span>
+            </p>
+          </div>
+          <div>Buttons</div>
+        </div>
+        <div
+          id="photo_n_links"
+          className={`flex w-full flex-row justify-between gap-[10px] border-0 border-red-400 ${
+            isSmall ? `order-1` : `order-2`
+          }`}
+        >
+          <div>
+            <img id="photo" src={bg_photo} alt="bg_logo"></img>
+          </div>
+          <div id="links" className="flex flex-col items-center justify-center gap-[10px] border-0 border-red-400">
+            <img src={icon_linkedin} alt="icon_linkedin" />
+            <img src={icon_github} alt="icon_github" />
+            <img src={icon_twitter} alt="icon_twitter" />
+          </div>
+        </div>
       </div>
       <div id="landing_banner_back" className=""></div>
     </div>
