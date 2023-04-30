1b9524392920c413b497e72ae9a91bb11635243d
Bulent Gercek <bulentgercek@gmail.com>
2023-04-30 19:22:05 +0300

    Fix...

--- _update_client.sh
+++ _update_client.sh
@@ -6,7 +6,9 @@ cd client
 npm run build && echo "Client is built."
 
 # Copy the client files to the Nginx directory
-sudo rm -rf /var/www/bulentgercek.com/client/* && \
+sudo rm -rf /var/www/bulentgercek.com/client/assets && \
+sudo rm /var/www/bulentgercek.com/client/favicon.ico && \
+sudo rm /var/www/bulentgercek.com/client/index.html && \
 sudo cp -r /home/bulentgercek/bg-portfolio/client/dist/* /var/www/bulentgercek.com/client/ && \
 echo "Client files are copied to the Nginx directory."
 
