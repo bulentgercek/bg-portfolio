561013eb3df9e5492cc87cff9fe24bf9f021f308
Bulent Gercek <bulentgercek@gmail.com>
2023-04-30 18:10:54 +0300

    Trying to fix env on vps

--- server/src/validEnv.ts
+++ server/src/validEnv.ts
@@ -1,13 +1,15 @@
 import * as dotenv from "dotenv";
+import * as path from "path";
 
 // Load .env file contents into process.env
 dotenv.config();
+console.log("Current working directory:", process.cwd());
+console.log("process.env.DATABASE_URL:", process.env.DATABASE_URL);
+console.log("process.env.PORT:", process.env.PORT);
 
 // Check if PORT environment variable ise defined or exit the process
 if (!process.env.DATABASE_URL || !process.env.PORT) {
-  console.error(
-    "ERROR: DATABASE_URL and PORT must be properly defined in .env file."
-  );
+  console.error("ERROR: DATABASE_URL and PORT must be properly defined in .env file.");
   process.exit(1);
 }
 
