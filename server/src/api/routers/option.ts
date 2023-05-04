84fc8946ed1c68e84b7c9c956d5263ae85cb1436
Bulent Gercek <bulentgercek@gmail.com>
2023-05-04 18:24:24 +0300

    Options was not updated :)

--- server/src/api/routers/option.ts
+++ server/src/api/routers/option.ts
@@ -2,87 +2,109 @@ import { Router } from "express";
 import { z } from "zod";
 import { ApiController as ac } from "../../apiController";
 import { Option, OptionCategory, OptionType } from "../../entities/Option";
+import { consoleRouteError } from "../../errorHandler";
 
 const router = Router();
 
 // Get all Options
 router.get("/", async (req, res, next) => {
-  const dbOptions = await ac.findAll(Option);
+  try {
+    const dbOptions = await ac.findAll(Option);
 
-  res.json(dbOptions);
+    res.json(dbOptions);
+  } catch (error) {
+    consoleRouteError(error, req);
+    next(error);
+  }
 });
 
 // Get spesific Option with id
 router.get("/:id", async (req, res, next) => {
-  const ctxObj = ac.initContext({
-    zInput: { params: z.object({ id: z.preprocess(Number, z.number()) }) },
-    reqData: { params: req.params },
-  });
+  try {
+    const validateResults = await ac.inputValidate({
+      zInput: { params: z.object({ id: z.preprocess(Number, z.number()) }) },
+      reqData: { params: req.params },
+    });
+    const dbOption = await ac.findOne(Option, {
+      where: {
+        id: validateResults.result.params.id,
+      },
+    });
 
-  const validateResults = await ac.inputValidate(ctxObj);
-  const dbOption = await ac.findOne(Option, validateResults);
-
-  res.json(dbOption);
+    res.json(dbOption);
+  } catch (error) {
+    consoleRouteError(error, req);
+    next(error);
+  }
 });
 
 // Add an Option
 router.post("/", async (req, res, next) => {
-  const ctxObj = ac.initContext({
-    zInput: {
-      body: z.object({
-        name: z.string().optional(),
-        category: z.nativeEnum(OptionCategory).optional(),
-        type: z.nativeEnum(OptionType).optional(),
-        value: z.string().optional(),
-        relation: z.number().optional(),
-      }),
-    },
-    reqData: { body: req.body },
-  });
+  try {
+    const validateResults = await ac.inputValidate({
+      zInput: {
+        body: z.object({
+          name: z.string().optional(),
+          category: z.nativeEnum(OptionCategory).optional(),
+          type: z.nativeEnum(OptionType).optional(),
+          value: z.string().optional(),
+          relation: z.number().optional(),
+        }),
+      },
+      reqData: { body: req.body },
+    });
+    const addedOption = await ac.addWithCreate(Option, validateResults);
 
-  const validateResults = await ac.inputValidate(ctxObj);
-  const addedOption = await ac.addWithCreate(Option, validateResults);
-
-  res.json(addedOption);
+    res.json(addedOption);
+  } catch (error) {
+    consoleRouteError(error, req);
+    next(error);
+  }
 });
 
 // Update the Option with id
 router.put("/:id", async (req, res, next) => {
-  const ctxObj = ac.initContext({
-    zInput: {
-      params: z.object({ id: z.preprocess(Number, z.number()) }),
-      body: z.object({
-        name: z.string().optional(),
-        category: z.nativeEnum(OptionCategory).optional(),
-        type: z.nativeEnum(OptionType).optional(),
-        value: z.string().optional(),
-        relation: z.number().optional(),
-      }),
-    },
-    reqData: { params: req.params, body: req.body },
-  });
+  try {
+    const validateResults = await ac.inputValidate({
+      zInput: {
+        params: z.object({ id: z.preprocess(Number, z.number()) }),
+        body: z.object({
+          name: z.string().optional(),
+          category: z.nativeEnum(OptionCategory).optional(),
+          type: z.nativeEnum(OptionType).optional(),
+          value: z.string().optional(),
+          relation: z.number().optional(),
+        }),
+      },
+      reqData: { params: req.params, body: req.body },
+    });
+    const updatedOption = await ac.update(Option, validateResults);
 
-  const validateResults = await ac.inputValidate(ctxObj);
-  const updatedOption = await ac.update(Option, validateResults);
-
-  res.json(updatedOption);
+    res.json(updatedOption);
+  } catch (error) {
+    consoleRouteError(error, req);
+    next(error);
+  }
 });
 
 // Remove an Option with id
 router.delete("/:id", async (req, res, next) => {
-  const ctxObj = ac.initContext({
-    zInput: {
-      params: z.object({
-        id: z.preprocess(Number, z.number()),
-      }),
-    },
-    reqData: { params: req.params },
-  });
+  try {
+    const validateResults = await ac.inputValidate({
+      zInput: {
+        params: z.object({
+          id: z.preprocess(Number, z.number()),
+        }),
+      },
+      reqData: { params: req.params },
+    });
+    const removedOption = await ac.remove(Option, validateResults);
 
-  const validateResults = await ac.inputValidate(ctxObj);
-  const removedOption = await ac.remove(Option, validateResults);
-
-  res.json(removedOption);
+    res.json(removedOption);
+  } catch (error) {
+    consoleRouteError(error, req);
+    next(error);
+  }
 });
 
 export { router as optionRouter };
