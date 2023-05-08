f349fa84f8eff1f007c4beaace6871898811c526
Bulent Gercek <bulentgercek@gmail.com>
2023-05-08 22:40:07 +0300

    Category content

--- client/src/layouts/Content/CategoryContentLayout.tsx
+++ client/src/layouts/Content/CategoryContentLayout.tsx
@@ -14,17 +14,14 @@ const CategoryContentLayout: React.FC = () => {
   }, [routeData]);
 
   return (
-    <div className="flex flex-col gap-2.5 rounded-2xl bg-blue-200 p-5">
+    <div className="flex w-full flex-col gap-2.5 rounded-2xl bg-blue-200 p-5">
       {/* Header */}
-      <div className="flex w-full flex-row rounded-xl bg-blue-100 p-5">
+      <div className="flex w-full flex-row flex-wrap justify-between rounded-xl bg-blue-100 p-5">
         <div className="flex flex-col gap-2">
           <Breadcrumbs pageType="Category" />
           <div>
             <div className="text-lg font-bold text-indigo-900">{currentCategory?.name}</div>
-            <div className="">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut vehicula sem, quis tincidunt
-              metus. Fusce at arcu nec tortor suscipit convallis non sit amet nulla.
-            </div>
+            <div className="">{currentCategory?.description}</div>
           </div>
         </div>
         <div className="flex min-w-[218px] p-5">Info Panel</div>
