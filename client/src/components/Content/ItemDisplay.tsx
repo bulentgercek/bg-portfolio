366ce8a03d3deadcb50ca0b62c64c821b0df8238
Bulent Gercek <bulentgercek@gmail.com>
2023-05-12 14:22:11 +0300

    line-clamp-5 for long item.descriptons

--- client/src/components/Content/ItemDisplay.tsx
+++ client/src/components/Content/ItemDisplay.tsx
@@ -24,7 +24,7 @@ const ItemDisplay: React.FC<ItemProps> = ({ item }) => {
   return (
     <>
       {(item && (
-        <div id="container" className="trans-d200 flex h-[500px] w-full flex-col rounded-2xl bg-indigo-50">
+        <div id="container" className="trans-d200 flex max-h-[500px] w-full flex-col rounded-2xl bg-indigo-50">
           <div id="image" className="flex h-[200px] items-center justify-center overflow-hidden rounded-t-2xl ">
             <img
               className="max-w-[420px]"
@@ -38,7 +38,9 @@ const ItemDisplay: React.FC<ItemProps> = ({ item }) => {
               {item.name}
             </div>
             <div id="description_n_button" className="flex h-full flex-col justify-between">
-              <div id="description">{item.description}</div>
+              <div className="line-clamp-5" id="description">
+                {item.description}
+              </div>
               <Link to={linkTo} key={item.id}>
                 <button
                   className={`trans-d500 flex h-[40px] items-center rounded-2xl bg-blue-600 px-5 py-3 text-base font-bold text-indigo-50 hover:px-6`}
