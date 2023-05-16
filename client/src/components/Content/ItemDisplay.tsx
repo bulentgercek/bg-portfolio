89489d260e6e4c71588c808ee1e86c2812fcacb1
Bulent Gercek <bulentgercek@gmail.com>
2023-05-16 21:21:10 +0300

    item display ui tweaks

--- client/src/components/Content/ItemDisplay.tsx
+++ client/src/components/Content/ItemDisplay.tsx
@@ -24,37 +24,41 @@ const ItemDisplay: React.FC<ItemProps> = ({ item }) => {
   return (
     <>
       {(item && (
-        <div id="container" className="trans-d200 flex max-h-[500px] w-full flex-col rounded-2xl bg-indigo-50">
-          <div id="image" className="flex h-[200px] items-center justify-center overflow-hidden rounded-t-2xl ">
-            <img
-              className="max-w-[420px]"
-              src={`${item.featuredImageAsset?.url}`}
-              alt="img"
-              crossOrigin="anonymous"
-            ></img>
-          </div>
-          <div id="info" className="flex flex-1 flex-col p-7 pt-6 text-indigo-900">
-            <div id="title" className="font-bold ">
-              {item.name}
+        <Link to={linkTo} key={item.id}>
+          <div
+            id="container"
+            className="trans-d500 flex h-[450px] w-full flex-col rounded-2xl bg-indigo-50 outline-none outline-2 outline-offset-0 outline-indigo-400/0 hover:translate-x-1 hover:bg-gray-100 hover:outline-gray-100/100"
+          >
+            <div id="image" className="flex h-[200px] items-center justify-center overflow-hidden rounded-t-2xl ">
+              <img
+                className="max-w-[420px]"
+                src={`${item.featuredImageAsset?.url}`}
+                alt="img"
+                crossOrigin="anonymous"
+              ></img>
             </div>
-            <div id="description_n_button" className="flex h-full flex-col justify-between">
-              <div className="line-clamp-5" id="description">
-                {item.description}
+            <div id="info" className="flex flex-1 flex-col p-7 pt-6 text-indigo-900">
+              <div id="title" className="font-bold ">
+                {item.name}
               </div>
-              <Link to={linkTo} key={item.id}>
-                <button
+              <div id="description_n_button" className="flex h-full flex-col justify-between">
+                <div className="line-clamp-4" id="description">
+                  {item.description}
+                </div>
+
+                {/* <button
                   className={`trans-d500 flex h-[40px] items-center rounded-2xl bg-blue-600 px-5 py-3 text-base font-bold text-indigo-50 hover:px-6`}
                 >
                   {`Go to Work`}
-                </button>
-              </Link>
+                </button> */}
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       )) ?? (
         <div
           id="container"
-          className="trans-d200 flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-2xl bg-blue-50/25"
+          className="trans-d200 flex h-[450px] w-full flex-col items-center justify-center overflow-hidden rounded-2xl bg-blue-50/25"
         >
           <img src={landing_banner_back} className="max-w-[368px]"></img>
         </div>
